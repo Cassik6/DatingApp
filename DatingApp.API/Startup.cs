2bf84f0581d776b705f4567295415c427a23888b
Cassi <opaps.ditudidi@gmail.com>
2020-05-18 06:02:28 +0200

    End of section 4

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -62,10 +62,10 @@ namespace DatingApp.API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());           
+
             app.UseAuthentication();
             app.UseAuthorization();
-
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             
 
